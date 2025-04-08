03be50596480cabdf7cd17ff8b62967068781aa7
Sanskar1823 <Sanskarshrivastav23@gmail.com>
2025-04-08 16:29:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function goToLogin() {
+  window.location.href = "login.html";
+}
+
+function login() {
+  // Replace with actual login logic
+  alert("Logging in...");
+  window.location.href = "dashboard.html";
+}
+
+function signup() {
+  alert("Signup logic here.");
+}
+
+function guestLogin() {
+  alert("Guest login successful.");
+  window.location.href = "dashboard.html";
+}
